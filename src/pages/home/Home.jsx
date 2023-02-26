1893c23b28bc78426d07c97bac610285dfa6a365
Kara <kara_himanshu@yahoo.com>
2023-02-26 23:41:25 +0530

    Added - Featured Section

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,8 +1,13 @@
 import React from "react";
+import Featured from "../../components/Featured/Featured";
 import "./Home.scss";
 
 const Home = () => {
-  return <div className="home">Home</div>;
+  return (
+    <div className="home">
+      <Featured />
+    </div>
+  );
 };
 
 export default Home;
